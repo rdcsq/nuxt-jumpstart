989160f5d4eb3f99f87afaec381e88d26031ffd6
rdcsq <git@rdcsq.me>
2024-09-04 10:21:30 -0700

    tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  css: ["assets/css/globals.css"],
+  modules: ["@nuxtjs/tailwindcss"],
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+    "./data/links.ts",
+  ],
+  plugins: [],
+} satisfies Config;
